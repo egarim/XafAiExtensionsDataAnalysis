3be08656c354003f5aea1cfb3f2c9d1182c66607
Jose Ojeda <joche.ojeda@bitframeworks.com>
2025-02-15 10:40:43 +0300

    Pivot schema documentation

--- XafAiExtensionsDataAnalysis/XafAiExtensionsDataAnalysis.Module/Ai/PivotChart/FilterSettings.cs
+++ XafAiExtensionsDataAnalysis/XafAiExtensionsDataAnalysis.Module/Ai/PivotChart/FilterSettings.cs
@@ -1,16 +1,36 @@
-﻿using System;
-using System.Linq;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 
+using System.Text.Json.Serialization;
 namespace XafAiExtensionsDataAnalysis.Module.Ai.PivotChart
 {
-    using System.Collections.Generic;
+   
+
+    [Description("Defines filtering and totals display configuration for a pivot field")]
     public class FilterSettings
     {
-        public string FilterType { get; set; } // Value, Set, TopN, etc.
+        [Description("Type of filter to apply (Value, Set, TopN)")]
+        [JsonPropertyName("filterType")]
+        public string FilterType { get; set; }
+
+        [Description("Value to use for filtering when FilterType is Value or TopN")]
+        [JsonPropertyName("filterValue")]
         public object FilterValue { get; set; }
+
+        [Description("Determines if grand totals should be displayed in the pivot view")]
+        [JsonPropertyName("showGrandTotals")]
         public bool ShowGrandTotals { get; set; } = true;
+
+        [Description("Determines if column totals should be displayed in the pivot view")]
+        [JsonPropertyName("showColumnTotals")]
         public bool ShowColumnTotals { get; set; } = true;
+
+        [Description("Determines if row totals should be displayed in the pivot view")]
+        [JsonPropertyName("showRowTotals")]
         public bool ShowRowTotals { get; set; } = true;
+
+        [Description("Collection of specific values to include/exclude when FilterType is Set")]
+        [JsonPropertyName("filteredValues")]
         public List<object> FilteredValues { get; set; }
     }
 }
--- XafAiExtensionsDataAnalysis/XafAiExtensionsDataAnalysis.Module/Ai/PivotChart/LayoutSettings.cs
+++ XafAiExtensionsDataAnalysis/XafAiExtensionsDataAnalysis.Module/Ai/PivotChart/LayoutSettings.cs
@@ -1,15 +1,35 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Linq;
+using System.Text.Json.Serialization;
 
 namespace XafAiExtensionsDataAnalysis.Module.Ai.PivotChart
 {
+    [Description("Defines the visual and behavioral layout settings for pivot field display")]
     public class LayoutSettings
     {
+        [Description("Width of the field column (can be pixel value or percentage)")]
+        [JsonPropertyName("width")]
         public string Width { get; set; }
+
+        [Description("Determines if the column width should automatically adjust to content")]
+        [JsonPropertyName("autoFitEnabled")]
         public bool AutoFitEnabled { get; set; }
+
+        [Description("Format string for displaying numeric values (e.g. '#,##0.00', 'P2')")]
+        [JsonPropertyName("numberFormat")]
         public string NumberFormat { get; set; }
+
+        [Description("Indicates if text should wrap to multiple lines when it exceeds column width")]
+        [JsonPropertyName("wordWrap")]
         public bool WordWrap { get; set; }
+
+        [Description("Text alignment within the column ('Left', 'Center', 'Right')")]
+        [JsonPropertyName("horizontalAlignment")]
         public string HorizontalAlignment { get; set; }
+
+        [Description("Specifies whether the field can be dragged to different areas in the pivot")]
+        [JsonPropertyName("allowDrag")]
         public bool AllowDrag { get; set; } = true;
     }
 }
